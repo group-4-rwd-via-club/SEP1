76cc85fcd7452d92d492553f3161b3c773cb239a
CHANHaochengZhang <285510@via.dk>
2019-05-23 12:26:30 +0200

    Update Player.java

--- src/application/classes/Player.java
+++ src/application/classes/Player.java
@@ -5,102 +5,18 @@ import java.util.Arrays;
 public class Player
 {
    private String id;
-   private String name;
+   private String firstname;
+   private String Lastname;
    private int number;
-   private String shirt;
-   private String position;
-   // Jesper says there is another way to do "Status"
-   // calling it status to reduce confusion with value "available"
-   // name can be changed if unacceptable
-   private String[] status = {"Available", "Injured"
-         , "Suspended", "Other"};
-   private int matches;
+   private String shirtName;
+   private String preferredPosition;
+   private int matchesInRow;
+ 
+ 
+ 
    
-   public Player(String name, int number, String shirt
-         , String position)
-   {
-      this.name = name;
-      this.number = number;
-      this.shirt = shirt;
-      this.position = position;
-      // to fix array
-      status = new String[0];
-      this.matches = 0;
-   }
-   
-   public Player()
-   {
-      this.name = "";
-      this.number = 0;
-      this.shirt = "";
-      this.position = "";
-      // to fix array
-      this.status = null;
-      this.matches = 0;
-   }
-
-   public String getName()
-   {
-      return name;
-   }
-
-   public void setName(String name)
-   {
-      this.name = name;
-   }
-
-   public int getNumber()
-   {
-      return number;
-   }
-
-   public void setNumber(int number)
-   {
-      this.number = number;
-   }
-
-   public String getShirt()
-   {
-      return shirt;
-   }
-
-   public void setShirt(String shirt)
-   {
-      this.shirt = shirt;
-   }
-
-   public String getPosition()
-   {
-      return position;
-   }
-
-   public void setPosition(String position)
-   {
-      this.position = position;
-   }
-
-   public String[] getStatus()
-   {
-      return status;
-   }
-
-   public void setStatus(String[] status)
-   {
-      this.status = status;
-   }
-
-   public int getMatches()
-   {
-      return matches;
-   }
-
-   public void setMatches(int matches)
-   {
-      this.matches = matches;
-   }
-   public String getId() {
-      return this.id;
-   }
+  
+ 
    
    public boolean copy(Object obj)
    {
