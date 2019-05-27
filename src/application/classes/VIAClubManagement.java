5213cadc92e0096a45dc5911425d7616cda780bf
VIA-JH <48440436+VIA-Jesper@users.noreply.github.com>
2019-05-27 21:51:03 +0200

    Update VIAClubManagement.java

--- src/application/classes/VIAClubManagement.java
+++ src/application/classes/VIAClubManagement.java
@@ -27,33 +27,12 @@ public class VIAClubManagement {
     {
         // TODO: Logic for loading from fileIO
 
-
+        // Test data loaded into the lists
         // Load test Data simulates data from file:
-        // Matches
-        Match match1 = new Match("Opponent1", MatchType.cup, "City1");
-        match1.setDate(new Date(1,6,2019));
-        match1.setScore("1:4");
-
-        Match match2 = new Match("Opponent2", MatchType.league, "City2");
-        match2.setDate(new Date(1,7,2019));
-
-        Match match3 = new Match("Opponent3", MatchType.friendly, "City3");
-        match3.setDate(new Date(1,8,2019));
-
-        Match match4 = new Match("Opponent1", MatchType.friendly, "City4");
-        match4.setDate(new Date(1,9,2019));
-        match4.setScore("3:1");
-
-        Match match5 = new Match("Opponent2", MatchType.cup, "City5");
-        match5.setDate(new Date(1,10,2019));
 
 
-        // adding to matchlist
-        matchList.addMatch(match1);
-        matchList.addMatch(match2);
-        matchList.addMatch(match3);
-        matchList.addMatch(match4);
-        matchList.addMatch(match5);
+
+
 
         // Players
         Player player1 = new Player("firstname1", "lastname1", 1, "Player1","Bench");
@@ -72,6 +51,32 @@ public class VIAClubManagement {
         playerList.addPlayer(player6);
 
 
+        // Matches
+        Match match1 = new Match("Opponent1", MatchType.cup, "City1");
+        match1.setDate(new Date(1,6,2019));
+        match1.setScore("1:4");
+        match1.setRoster(playerList);
+
+        Match match2 = new Match("Opponent2", MatchType.league, "City2");
+        match2.setDate(new Date(1,7,2019));
+
+        Match match3 = new Match("Opponent3", MatchType.friendly, "City3");
+        match3.setDate(new Date(1,8,2019));
+        match3.setRoster(playerList);
+
+        Match match4 = new Match("Opponent1", MatchType.friendly, "City4");
+        match4.setDate(new Date(1,9,2019));
+        match4.setScore("3:1");
+
+        Match match5 = new Match("Opponent2", MatchType.cup, "City5");
+        match5.setDate(new Date(1,10,2019));
+
+        // adding to matchlist
+        matchList.addMatch(match1);
+        matchList.addMatch(match2);
+        matchList.addMatch(match3);
+        matchList.addMatch(match4);
+        matchList.addMatch(match5);
     }
 
 
