2c0b4348eea8a40e3f2eb31510922cba2f8ed4d9
CHANHaochengZhang <285510@via.dk>
2019-05-28 15:56:58 +0200

    Update PlayerListViewController.java

--- src/application/controllers/PlayerListViewController.java
+++ src/application/controllers/PlayerListViewController.java
@@ -5,6 +5,8 @@ import application.classes.Match;
 import application.classes.Player;
 import application.classes.PlayerList;
 import application.views.PlayerViewClass;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -32,27 +34,36 @@ public class PlayerListViewController
 
    // The reference of tableView will be injected by the FXML loader
    @FXML
-   private TableView tableView;
-   
-   // table 
-   @FXML 
+   private TableView table;
+
+   // table
+   @FXML
    private TableColumn<Player, String> firstname;
-   
+
    @FXML
    private TableColumn<Player, String> lastname;
    
    @FXML
+   private TableColumn<Player, Number> number;
+
+   @FXML
    private TableColumn<Player, String> shirt;
-   
+
    @FXML
    private TableColumn<Player, String> position;
-   
+
    @FXML
    private TableColumn<Availability, String> status;
    
- //TODO How many matches a player were played
-   
-   
+   private PlayerList p;
+
+   // TODO How many matches a player were played
+
+   @FXML
+   private ObservableList<Player> data = FXCollections.observableArrayList(
+               new Player("Haocheng", "Zhang", 10, "HZ", "RF")
+
+         );
 
    public PlayerListViewController()
    {
@@ -64,11 +75,8 @@ public class PlayerListViewController
 
    {
       PlayerList p = new PlayerList();
-      firstname = new TableColumn<Player, String>();
-      lastname= new TableColumn<Player, String>();
-      shirt = new TableColumn<Player, String>();
-      position = new TableColumn<Player, String>();
-      status = new TableColumn<Availability, String>();
+
+      
 
       // add
       add.setOnAction(event -> {
@@ -88,21 +96,63 @@ public class PlayerListViewController
                   Alert alert = new Alert(AlertType.INFORMATION);
                   alert.setTitle("Players");
                   alert.setHeaderText(null);
-               
-                     alert.setContentText(p.toString());
-                  
+
+                  alert.setContentText(p.toString());
+
                   alert.showAndWait();
 
                }
             }
 
       );
-      
-      // search by keyword
-      
-      
-      
-      
 
+      //TODO search by keyword
+      
+      //list
+      table = new TableView<>();
+      firstname = new TableColumn<Player, String>();
+      lastname = new TableColumn<Player, String>();
+      
+      shirt = new TableColumn<Player, String>();
+      position = new TableColumn<Player, String>();
+      status = new TableColumn<Availability, String>();
+      
+      table.setEditable(true);
+      table.setItems(data);
+      table.getColumns().addAll(firstname,lastname,shirt,position,status);
+      
+      
+       }
+   
+   
+   private ObservableList<Player> filteredData = FXCollections.observableArrayList();
+   private void setFilteredData(String keyword)
+   {
+      
+      
+       // if keyword is empty, display the entire dataset
+       if (keyword.isEmpty())
+       {
+           // set table data to the master data set.
+           table.setItems(data);
+       }
+       // Clear after each keyword is being typed
+       filteredData.clear();
+
+
+
+       // loop through dataset to find keyword
+       for (Player match : p.getAllPlayers())
+       {
+           // Java String indexOf() The java string indexOf() method returns index of given character value or substring. If it is not found, it returns -1. The index counter starts from zero.
+           if (match.toString().toLowerCase().indexOf(keyword.toLowerCase()) != -1)
+           {
+               // added to filtered match list
+               filteredData.add(match);
+           }
+       }
+       // set table data to filtered data.
+       table.setItems(filteredData);
    }
+   
 }
